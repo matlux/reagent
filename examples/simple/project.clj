f769d04994be81dba5b8814528b77795841edd3a
Mathieu Gauthron <mathieu@matlux.net>
2017-11-25 14:50:33 +0000

    simple project with extra component and with nrepl

--- examples/simple/project.clj
+++ examples/simple/project.clj
@@ -3,7 +3,9 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.107"]
                  [reagent "0.6.0"]
-                 [figwheel "0.3.7"]]
+                 [figwheel "0.3.7"]
+                 [cljs-ajax "0.7.3"]
+                 [cljs-http "0.1.44"]]
 
   :plugins [[lein-cljsbuild "1.0.6"]
             [lein-figwheel "0.3.7"]]
@@ -11,10 +13,15 @@
   :hooks [leiningen.cljsbuild]
 
   :profiles {:dev {:cljsbuild
-                   {:builds {:client
-                             {:figwheel {:on-jsload "simpleexample.core/run"}
-                              :compiler {:main "simpleexample.core"
-                                         :optimizations :none}}}}}
+                                 {:builds {:client
+                                           {:figwheel {:on-jsload "simpleexample.core/run"}
+                                            :compiler {:main          "simpleexample.core"
+                                                       :optimizations :none}}}}
+                   :dependencies [[com.cemerick/piggieback "0.2.1"]
+                                  [figwheel-sidecar "0.5.8"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+                   ;:repl-options {:nrepl-middleware [figwheel.tools.nrepl/wrap-cljs-repl]}
+                   }
 
              :prod {:cljsbuild
                     {:builds {:client
--- examples/simple/src/simpleexample/core.cljs
+++ examples/simple/src/simpleexample/core.cljs
@@ -1,12 +1,66 @@
 (ns simpleexample.core
-  (:require [reagent.core :as r]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs-http.client :as http]
+            [cljs.core.async :refer [<! take!]]
+            [ajax.core :refer [GET POST]]
+            [reagent.core :as r]))
+
+(comment
+
+  (use 'figwheel-sidecar.repl-api)
+  (start-figwheel!)
+  (cljs-repl)
+
+  (in-ns 'simpleexample.core)
+  *ns*
+  (println "hiya2")
+  (http/get "https://api.github.com/users"
+            {:with-credentials? false
+             :query-params      {"since" 135}})
+
+  (def c (go (let [response (<! (http/get "https://api.github.com/users"
+                                          {:with-credentials? false
+                                           :query-params      {"since" 135}}))]
+               (prn (:status response))
+               (prn (map :login (:body response))))))
+  (take! (go (let [response (<! (http/get "https://api.github.com/users"
+                                          {:with-credentials? false
+                                           :query-params      {"since" 135}}))]
+               (prn (:status response))
+               (prn (map :login (:body response)))))
+         #(println %))
+
+  (take! c #(println %))
+(GET "http://localhost:8080/bar")
+  (foo)
+
+  )
+
+(enable-console-print!)
+
+(defn foo2 []
+  (take! (go (let [response (<! (http/get "https://api.github.com/users"
+                                          {:with-credentials? false
+                                           :query-params      {"since" 135}}))]
+               (prn (:status response))
+               (prn (map :login (:body response)))))
+         #(println %)))
+(defn foo []
+  (take! (go (let [response (<! (http/get "http://localhost:8080/bar"
+                                          {:with-credentials? false}))]
+               (prn (:status response))
+               (prn (map :login (:body response)))))
+         #(println %)))
 
 (defonce timer (r/atom (js/Date.)))
 
-(defonce time-color (r/atom "#f34"))
+(def time-color (r/atom "#920"))
+(def test-field (r/atom "data..."))
 
-(defonce time-updater (js/setInterval
-                       #(reset! timer (js/Date.)) 1000))
+(def time-updater (js/setInterval
+                        #(do
+                           (reset! test-field (foo))
+                           (reset! timer (js/Date.))) 5000))
 
 (defn greeting [message]
   [:h1 message])
@@ -17,6 +71,7 @@
      {:style {:color @time-color}}
      time-str]))
 
+
 (defn color-input []
   [:div.color-input
    "Time color: "
@@ -24,11 +79,19 @@
             :value @time-color
             :on-change #(reset! time-color (-> % .-target .-value))}]])
 
+(defn test-component []
+  [:div.color-input
+   "test="
+   @test-field
+   ])
+
+
 (defn simple-example []
   [:div
-   [greeting "Hello world, it is now"]
+   [greeting "Hello world2, it is now"]
    [clock]
-   [color-input]])
+   [color-input]
+   [test-component]])
 
 (defn ^:export run []
   (r/render [simple-example]
